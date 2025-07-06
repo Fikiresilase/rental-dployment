9120d68d02d7271b28c6542e14a03aed2ca2afe3
Fikiresilase <noahkelemework@gmail.com>
2025-07-06 11:31:56 +0300

    smtp email change

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
   host: 'gmail',
   auth: {
     user: 'noahkelemework@gmail.com',
-    pass: 'arjjjllnqsjoozlk'
+    pass: 'rcqx wkrh dxzx tsrv'
 
   },
 });
